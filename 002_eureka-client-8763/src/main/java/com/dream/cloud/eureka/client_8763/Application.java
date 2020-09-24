bbadef0349f6a94fd710b79630e1b07fc26cc458
1134005157 <1134005157@qq.com>
2020-09-24 17:16:26 +0800

    eureka

---
+++ 002_eureka-client-8763/src/main/java/com/dream/cloud/eureka/client_8763/Application.java
@@ -0,0 +1,13 @@
+package com.dream.cloud.eureka.client_8763;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}

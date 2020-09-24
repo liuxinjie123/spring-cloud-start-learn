ad879043f8c003ba069c87d31724984412faeffa
1134005157 <1134005157@qq.com>
2020-09-24 16:35:57 +0800

    eureka

---
+++ 001_eureka-client/src/test/java/com/dream/cloud/eureka/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.cloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ 001_eurker-server/src/test/java/com/dream/cloud/enreka/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.cloud.enreka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

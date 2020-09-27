cfd7ab10bfd5e05fa70e5aecdbe0757b746641d8
1134005157 <1134005157@qq.com>
2020-09-27 23:26:02 +0800

    config server

---
+++ 007_eureka-server-config/src/main/java/com/dream/cloud/config/eureka_server/EurekaServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.dream.cloud.config.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerConfigApplication.class, args);
+    }
+
+}
---
+++ 007_eureka-server-config/src/test/java/com/dream/cloud/config/eureka_server/EurekaServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.cloud.config.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

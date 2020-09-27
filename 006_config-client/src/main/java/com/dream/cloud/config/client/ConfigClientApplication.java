291b22083f39b31f1b31a441f71b16ba06bf0507
1134005157 <1134005157@qq.com>
2020-09-27 23:45:24 +0800

    config server

--- 006_config-client/src/main/java/com/dream/cloud/config/client/ConfigClientApplication.java
+++ 006_config-client/src/main/java/com/dream/cloud/config/client/ConfigClientApplication.java
@@ -3,22 +3,18 @@ package com.dream.cloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
 
-    @Value("${foo}")
-    String foo;
 
-    @GetMapping(value = "/hello")
-    public String hi(){
-        return foo;
-    }
 }
